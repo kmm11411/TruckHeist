8842aebd3b8b1c1222507ac95a5f5afc7ac886c5
Kyle Moore <56to74@gmail.com>
2020-06-29 21:54:47 -0400

    no message

--- TruckHeist/Assets/Scripts/CarController.cs
+++ TruckHeist/Assets/Scripts/CarController.cs
@@ -35,12 +35,30 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if(m_CarAILogic.m_hitTruckFront || m_CarAILogic.m_hitTruckLeft || m_CarAILogic.m_hitTruckRight) {
+    //    if(m_CarAILogic.m_hitTruckFront || m_CarAILogic.m_hitTruckLeft || m_CarAILogic.m_hitTruckRight) {
+    //     //    if(fraction < 1) {
+    //     //         fraction += Time.deltaTime * 0.4f;
+    //     //         transform.position = Vector3.Lerp(transform.position, m_grappleLeftFollowSphere.transform.position, fraction);
+    //     //    }
+    //     transform.parent = m_truck.transform;
+    //     transform.localPosition = Vector3.MoveTowards(transform.localPosition, m_grappleLeftFollowSphere.transform.localPosition, 0.9f);
+           
+    //        //transform.parent = m_truck.transform;
+    //     //    if(m_CarAILogic.m_hitTruckLeft) {
+    //     //        MoveToGrapplePoint(m_grappleLeftFollowSphere.transform.position);
+    //     //    } else if (m_CarAILogic.m_hitTruckRight) {
+    //     //         MoveToGrapplePoint(m_grappleRightFollowSphere.transform.position);
+    //     //    }
+    //    }
+    }
+
+    private void FixedUpdate() {
+        if(m_CarAILogic.m_hitTruckFront || m_CarAILogic.m_hitTruckLeft || m_CarAILogic.m_hitTruckRight) {
         //    if(fraction < 1) {
         //         fraction += Time.deltaTime * 0.4f;
         //         transform.position = Vector3.Lerp(transform.position, m_grappleLeftFollowSphere.transform.position, fraction);
         //    }
-
+        transform.parent = m_truck.transform;
         transform.position = Vector3.MoveTowards(transform.position, m_grappleLeftFollowSphere.transform.position, 0.9f);
            
            //transform.parent = m_truck.transform;
