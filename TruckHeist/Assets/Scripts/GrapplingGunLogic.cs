d78e2f7df66b52ce4bf6f91358f82396a9bb9917
jlisooey <jlisooey@gmail.com>
2020-07-03 14:49:42 -0400

    Grappling Hook update, updates to truck, car, grappling gun prefabs

--- TruckHeist/Assets/Scripts/GrapplingGunLogic.cs
+++ TruckHeist/Assets/Scripts/GrapplingGunLogic.cs
@@ -13,59 +13,127 @@ public class GrapplingGunLogic : MonoBehaviour
     private LineRenderer m_lineRenderer;
     public GameObject m_GrapplingPlayer;
 
+    public float m_GravityDelay = .1f;
+    public float m_ReloadDelay = 4.0f;
     private float m_LookVerticalAngle = 0;
     private float m_LookHorizontalAngle = 0;
     public CinemachineVirtualCamera m_GrapplingHookCam;
+    private GrapplingHookLogic m_GrapplingHookLogic;
 
+    static float GRAVITYDELAY = .1f;
+    static float RELOADDELAY = 4.0f;
     CarAILogic m_carAILogic;
     TruckAILogic m_truckAILogic;
-
+    SteeringController m_SteeringController;
     CarController m_carController;
-
+    CapsuleCollider m_CapsuleCollider;
     // Start is called before the first frame update
     void Start()
     {
+        m_GrapplingHookLogic = m_grapplingHook.GetComponent<GrapplingHookLogic>();
         m_lineRenderer = GetComponent<LineRenderer>();
+        m_CapsuleCollider = m_grapplingHook.GetComponent<CapsuleCollider>();
         m_carAILogic = GetComponentInParent<CarAILogic>();
-        m_truckAILogic = GameObject.FindGameObjectWithTag("Truck").GetComponent<TruckAILogic>(); 
+        m_SteeringController = GetComponentInParent<SteeringController>();
+       // m_truckAILogic = GameObject.FindGameObjectWithTag("Truck").GetComponent<TruckAILogic>(); 
         m_carController = GetComponentInParent<CarController>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(m_carController.m_freezeGrapple) {
+
+
+
+
+        if (m_carController.m_freezeGrapple)
+        {
             m_lineRenderer.SetPosition(0, m_grapplingHookSpawn.transform.position);
             m_lineRenderer.SetPosition(1, m_grapplingHook.transform.position);
             return;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (m_GrapplingHookLogic.m_hitTruck)
         {
-            if(m_carAILogic.m_hitTruckLeft) {
-                m_carAILogic.m_hitTruckLeft = false;
-                m_truckAILogic.m_hitOnLeft = false;
-            } else if (m_carAILogic.m_hitTruckRight) {
-                m_carAILogic.m_hitTruckRight = false;
-                m_truckAILogic.m_hitOnRight = false;
-            } else if (m_carAILogic.m_hitTruckFront) {
-                m_carAILogic.m_hitTruckFront = false;
-                m_truckAILogic.m_hitOnFront = false;
-            }
+            m_lineRenderer.SetPosition(0, m_grapplingHookSpawn.transform.position);
+            m_lineRenderer.SetPosition(1, m_grapplingHook.transform.position);
 
-            m_gunFired = true;
-            m_grapplingHook.SetActive(true);
-            m_lineRenderer.enabled = true;
-            m_grapplingHookRigidBody = m_grapplingHook.GetComponent<Rigidbody>();
+            m_LookVerticalAngle -= Input.GetAxis("Mouse Y");
+            m_LookHorizontalAngle += Input.GetAxis("Mouse X");
+            m_LookVerticalAngle = Mathf.Clamp(m_LookVerticalAngle, -89f, 89f);
+
+            // apply the vertical angle as a local rotation to the camera transform along its right axis (makes it pivot up and down)
+            m_GrapplingPlayer.transform.localRotation = Quaternion.Euler(new Vector3(m_LookVerticalAngle, m_LookHorizontalAngle, 0));
+            return;
+        }
+
+        if (m_gunFired)
+        {
+            m_GravityDelay -= Time.fixedDeltaTime;
+            m_ReloadDelay -= Time.fixedDeltaTime;
+        }
+
+        if (m_GravityDelay < 0)
+        {
             m_grapplingHookRigidBody.useGravity = true;
-            m_grapplingHook.transform.parent = null;
-            
+            m_GravityDelay = GRAVITYDELAY;
+            m_CapsuleCollider.enabled = true;
+        }
+        if (m_ReloadDelay < 0)
+        {
+            m_CapsuleCollider.enabled = false;
+            m_grapplingHook.transform.parent = gameObject.transform;
+            m_grapplingHookRigidBody.useGravity = false;
+            m_GrapplingHookLogic.m_fired = false;
             m_grapplingHook.transform.position = m_grapplingHookSpawn.transform.position;
             m_grapplingHook.transform.rotation = m_grapplingHookSpawn.transform.rotation;
             var forwardDirection = transform.forward;
             m_grapplingHookRigidBody.velocity = Vector3.zero;
-            m_grapplingHookRigidBody.AddForce(forwardDirection * m_gunForce);
-        } //else if (Input.GetKeyDown(KeyCode.Space) && m_gunFired) {
+            m_grapplingHookRigidBody.angularVelocity = Vector3.zero;
+            m_gunFired = false;
+            m_ReloadDelay = RELOADDELAY;
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && !m_gunFired)
+        {
+            if (m_carAILogic.m_hitTruckLeft)
+            {
+                m_carAILogic.m_hitTruckLeft = false;
+                m_truckAILogic.m_hitOnLeft = false;
+            }
+            else if (m_carAILogic.m_hitTruckRight)
+            {
+                m_carAILogic.m_hitTruckRight = false;
+                m_truckAILogic.m_hitOnRight = false;
+            }
+            else if (m_carAILogic.m_hitTruckFront)
+            {
+                m_carAILogic.m_hitTruckFront = false;
+                m_truckAILogic.m_hitOnFront = false;
+            }
+            m_ReloadDelay = RELOADDELAY;
+            m_GravityDelay = GRAVITYDELAY;
+            m_gunFired = true;
+            m_grapplingHook.SetActive(true);
+            m_lineRenderer.enabled = true;
+            m_grapplingHookRigidBody = m_grapplingHook.GetComponent<Rigidbody>();
+           // m_grapplingHookRigidBody.useGravity = true;
+            m_grapplingHook.transform.parent = null;
+            m_grapplingHookRigidBody.useGravity = false;
+            m_GrapplingHookLogic.m_fired = true;
+            m_grapplingHook.transform.position = m_grapplingHookSpawn.transform.position;
+            m_grapplingHook.transform.rotation = m_grapplingHookSpawn.transform.rotation;
+            var forwardDirection = transform.forward;
+            m_grapplingHookRigidBody.angularVelocity = Vector3.zero;
+            m_grapplingHookRigidBody.velocity = Vector3.zero;
+
+            var appliedForce = m_gunForce + Mathf.Pow((m_SteeringController.m_velocityMagnitude/2),2);
+            Debug.Log(appliedForce);
+            m_grapplingHookRigidBody.AddForce(forwardDirection * appliedForce);
+        } 
+        
+        //else if (Input.GetKeyDown(KeyCode.Space) && m_gunFired) {
         //     m_gunFired = false;
         //     m_grapplingHook.SetActive(false);
         //     m_lineRenderer.enabled = false;
@@ -80,11 +148,8 @@ public class GrapplingGunLogic : MonoBehaviour
         //     // m_grapplingHookRigidBody.AddForce(forwardDirection * m_gunForce);
         // }
 
-        if (m_gunFired)
-        {
-            m_lineRenderer.SetPosition(0, m_grapplingHookSpawn.transform.position);
-            m_lineRenderer.SetPosition(1, m_grapplingHook.transform.position);
-        }
+        m_lineRenderer.SetPosition(0, m_grapplingHookSpawn.transform.position);
+        m_lineRenderer.SetPosition(1, m_grapplingHook.transform.position);
 
         m_LookVerticalAngle -= Input.GetAxis("Mouse Y");
         m_LookHorizontalAngle += Input.GetAxis("Mouse X");
--- TruckHeist/Assets/Scripts/GrapplingHookLogic.cs
+++ TruckHeist/Assets/Scripts/GrapplingHookLogic.cs
@@ -5,64 +5,127 @@ using UnityEngine;
 public class GrapplingHookLogic : MonoBehaviour
 {
     public bool m_hitTruck = false;
-
+    public bool m_fired = false;
     private Rigidbody m_RigidBody;
+    private CapsuleCollider m_CapsuleCollider;
+
 
     TruckAILogic m_truckAILogic;
     CarAILogic m_carAILogic;
-
+    // public GameObject m_grapplingHookSpawn;
     GameObject m_hitZoneLeftGrapplingPoint;
     GameObject m_hitZoneRightGrapplingPoint;
     GameObject m_hitZoneFrontGrapplingPoint;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
         m_RigidBody = GetComponent<Rigidbody>();
-        
+        m_CapsuleCollider = GetComponent<CapsuleCollider>();
         m_truckAILogic = GameObject.FindGameObjectWithTag("Truck").GetComponent<TruckAILogic>();
         m_carAILogic = GetComponentInParent<CarAILogic>();
 
         m_hitZoneLeftGrapplingPoint = GameObject.FindGameObjectWithTag("HitZoneLeftGrapplingPoint");
         m_hitZoneRightGrapplingPoint = GameObject.FindGameObjectWithTag("HitZoneRightGrapplingPoint");
         m_hitZoneFrontGrapplingPoint = GameObject.FindGameObjectWithTag("HitZoneFrontGrapplingPoint");
+        
+    }
+
+    //private void Update()
+    //{
+    //    if (!m_fired)
+    //    {
+    //        transform.position = m_grapplingHookSpawn.transform.position;
+    //        transform.rotation = m_grapplingHookSpawn.transform.rotation;
+    //    }
+    //}
+    private void FixedUpdate()
+    {
+        //if (m_fired)
+        //{
+        //    Debug.Log("m_RigidBody.velocity.normalized" + m_RigidBody.velocity.normalized * 360);
+        //    transform.rotation = Quaternion.Euler(m_RigidBody.velocity.normalized *360);
+        //}
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //if (m_hitTruck)
+        //{
+        //    Debug.Log(m_hitTruck);
+        //    m_CapsuleCollider.enabled = false;
+        //}
+        Debug.Log(other.tag);
         transform.position -= m_RigidBody.velocity.normalized * 2f;
-
-        Ray ray = new Ray(transform.position, m_RigidBody.velocity.normalized * 3f);
-        RaycastHit rayHit;
-
-        if (Physics.Raycast(ray, out rayHit, 3f))
+ 
+        if (other.tag == "HitZoneLeft" && !m_truckAILogic.m_hitOnLeft)
         {
+            //m_carAILogic.m_hitTruckLeft = true;
+            //m_truckAILogic.m_hitOnLeft = true;
+            m_hitTruck = true;
+            transform.position = m_hitZoneLeftGrapplingPoint.transform.position;
             m_RigidBody.velocity = Vector3.zero;
             m_RigidBody.useGravity = false;
-            if(other.tag == "HitZoneLeft" && !m_truckAILogic.m_hitOnLeft) {
-                m_carAILogic.m_hitTruckLeft = true;
-                m_truckAILogic.m_hitOnLeft = true;
-                transform.position = m_hitZoneLeftGrapplingPoint.transform.position;
-            } else if (other.tag == "HitZoneRight" && !m_truckAILogic.m_hitOnRight) {
-                m_carAILogic.m_hitTruckRight = true;
-                m_truckAILogic.m_hitOnRight = true;
-                transform.position = m_hitZoneRightGrapplingPoint.transform.position;
-            } else if (other.tag == "HitZoneFront" && !m_truckAILogic.m_hitOnFront && m_truckAILogic.m_hitOnLeft && m_truckAILogic.m_hitOnLeft) {
-                m_carAILogic.m_hitTruckFront = true;
-                m_truckAILogic.m_hitOnFront = true;
-                transform.position = m_hitZoneFrontGrapplingPoint.transform.position;
-            } else {
-                transform.position = rayHit.point;
-            }
+            m_CapsuleCollider.enabled = false;
+            m_RigidBody.isKinematic = true;
+            transform.parent = m_hitZoneLeftGrapplingPoint.transform;
         }
-        else
+        else if(other.tag == "HitZoneRight" && !m_truckAILogic.m_hitOnRight)
         {
+            //m_carAILogic.m_hitOnRight = true;
+            //m_truckAILogic.m_hitOnRight = true;
+            m_hitTruck = true;
+            transform.position = m_hitZoneRightGrapplingPoint.transform.position;
             m_RigidBody.velocity = Vector3.zero;
             m_RigidBody.useGravity = false;
+            m_RigidBody.isKinematic = true;
+            m_CapsuleCollider.enabled = false;
+            transform.parent = m_hitZoneRightGrapplingPoint.transform;
+        }
+        else if (other.tag == "HitZoneFront" && !m_truckAILogic.m_hitOnFront)
+        {
+            //m_carAILogic.m_hitTruckFront = true;
+            //m_truckAILogic.m_hitOnFront = true;
+            m_hitTruck = true;
+            transform.position = m_hitZoneFrontGrapplingPoint.transform.position;
+            m_RigidBody.velocity = Vector3.zero;
+            m_RigidBody.useGravity = false;
+            m_RigidBody.isKinematic = true;
+            m_CapsuleCollider.enabled = false;
+            transform.parent = m_hitZoneFrontGrapplingPoint.transform;
         }
 
-        transform.parent = other.transform;
+        //Ray ray = new Ray(transform.position, m_RigidBody.velocity.normalized * 3f);
+        //RaycastHit rayHit;
+
+
+
+        //if (Physics.Raycast(ray, out rayHit, 3f))
+        //{
+        //    m_RigidBody.velocity = Vector3.zero;
+        //    m_RigidBody.useGravity = false;
+        //    if(other.tag == "HitZoneLeft" && !m_truckAILogic.m_hitOnLeft) {
+        //        //m_carAILogic.m_hitTruckLeft = true;
+        //        //m_truckAILogic.m_hitOnLeft = true;
+        //        transform.position = m_hitZoneLeftGrapplingPoint.transform.position;
+        //    } else if (other.tag == "HitZoneRight" && !m_truckAILogic.m_hitOnRight) {
+        //        //m_carAILogic.m_hitTruckRight = true;
+        //        //m_truckAILogic.m_hitOnRight = true;
+        //        transform.position = m_hitZoneRightGrapplingPoint.transform.position;
+        //    } else if (other.tag == "HitZoneFront" && !m_truckAILogic.m_hitOnFront && m_truckAILogic.m_hitOnLeft && m_truckAILogic.m_hitOnLeft) {
+        //        //m_carAILogic.m_hitTruckFront = true;
+        //        //m_truckAILogic.m_hitOnFront = true;
+        //        transform.position = m_hitZoneFrontGrapplingPoint.transform.position;
+        //    } else {
+        //        transform.position = rayHit.point;
+        //    }
+        //}
+        //else
+        //{
+        //    m_RigidBody.velocity = Vector3.zero;
+        //    m_RigidBody.useGravity = false;
+        //}
+
+        //transform.parent = other.transform;
     }
 }
